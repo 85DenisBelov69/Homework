c1e15c1edabaede30a94d5636175acfc8912a681
DenisBelov <olgabelova@192.168.0.4>
2023-01-28 19:51:38 +0700

    task2 Final version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число А ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"Max = {a}");
+}
+else
+{
+    Console.WriteLine($"Max = {b}");
+}
