3a503366828d63334d21cb1074a967004b750fdc
DenisBelov <olgabelova@192.168.0.4>
2023-01-28 20:07:57 +0700

    add correct for min

--- Task2Final/Program.cs
+++ Task2Final/Program.cs
@@ -6,8 +6,10 @@ int b = Convert.ToInt32(Console.ReadLine());
 if (a > b)
 {
     Console.WriteLine($"Max = {a}");
+    Console.WriteLine($"Min = {b}");
 }
 else
 {
     Console.WriteLine($"Max = {b}");
+    Console.WriteLine($"Min = {a}");
 }
