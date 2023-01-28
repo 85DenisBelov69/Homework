8f7245859f10e2e9f51a28579f0cc89731025bb2
DenisBelov <olgabelova@192.168.0.4>
2023-01-28 21:40:16 +0700

    rename folders

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число А ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"Max = {a}");
+    Console.WriteLine($"Min = {b}");
+}
+else
+{
+    Console.WriteLine($"Max = {b}");
+    Console.WriteLine($"Min = {a}");
+}
