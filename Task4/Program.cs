f8c5657ed47c887a94ae0529d8f9928043798771
DenisBelov <olgabelova@192.168.0.4>
2023-01-28 20:56:01 +0700

    task4 version1

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число А ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+     max = b;   
+      
+}
+
+if (c > max)
+{
+    max = c;
+    Console.WriteLine($"Max = {max}");
+}
+
