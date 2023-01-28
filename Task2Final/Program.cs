1df5b0768e95390722661ac04b05496227946c09
DenisBelov <olgabelova@192.168.0.4>
2023-01-28 20:03:50 +0700

    add task2Final

---
+++ Task2Final/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число А ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"Max = {a}");
+}
+else
+{
+    Console.WriteLine($"Max = {b}");
+}
