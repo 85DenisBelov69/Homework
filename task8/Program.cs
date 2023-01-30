30f91900de789e0781d241a4ac8c1e54c024e62d
DenisBelov <olgabelova@192.168.0.4>
2023-01-30 12:53:39 +0700

    add task8 final

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = 1;
+
+while (b <= a)
+{
+    if (b%2 == 0)
+    Console.WriteLine($"{b}");
+    b++;
+    
+}
