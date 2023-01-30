0692fe21ee14bee0bd0b9ebebf304b06e6c48f8d
DenisBelov <olgabelova@192.168.0.4>
2023-01-30 12:33:24 +0700

    task6 final

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A%2! == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
+
