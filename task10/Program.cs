055fcae0def309fc576b16a5a6c142b43a4947e5
DenisBelov <olgabelova@192.168.0.4>
2023-02-03 11:44:58 +0700

    add task 10 final

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = (a/10 - a / 100 * 10);
+
+Console.WriteLine($"{b}");
