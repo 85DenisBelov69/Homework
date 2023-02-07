4b484a2dc8ad30930849a1759b8d965443cf7b7c
DenisBelov <olgabelova@192.168.0.4>
2023-02-07 12:20:49 +0700

    add task19 final

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите пятизначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a / 10000) == (a % 10) || (a / 1000 == a % 100))
+{
+Console.WriteLine("Это полиндром");
+}
+else
+{
+    Console.WriteLine("это не полиндром");
+}
