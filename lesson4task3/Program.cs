d6ce8a1f7aa740d22342eda8b7281c52a20282b8
DenisBelov <olgabelova@192.168.0.4>
2023-02-07 14:30:45 +0700

    add lesson4task3 final

--- lesson4task3/Program.cs
+++ lesson4task3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int s = 0;
+int b = 2;
+
+while (b <= a)
+{
+    s = b * b * b;
+    b = b + 2;
+    Console.Write($"{s}, ");
+}
+
