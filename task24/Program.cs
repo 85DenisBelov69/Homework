605aeb4931647128e83204c4cab76f12e6bfb6d5
DenisBelov <olgabelova@192.168.0.4>
2023-02-07 13:12:39 +0700

    add task24

---
+++ task24/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, 
+//которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int s = 0;
+int i ;
+for (i = 1; i <= a; i++)
+{
+    s = i * i * i;
+    
+    
+
+        Console.Write($"{s},  ");
+    
+}
