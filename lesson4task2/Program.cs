c261cb945da975a69e9785187a1b0c0cdbadb759
DenisBelov <olgabelova@192.168.0.4>
2023-02-07 14:15:54 +0700

    add lesson4task2 final

--- lesson4task2/Program.cs
+++ lesson4task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int s = 1;
+
+int b = 1;
+
+while (b <=  a)
+{
+    s = s * b;
+    b++;
+  
+}
+Console.WriteLine($"{s}"); 
