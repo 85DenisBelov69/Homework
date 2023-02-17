adbcb3edca6586134d31e2d0bf7231e98f7354f6
DenisBelov <olgabelova@192.168.0.4>
2023-02-17 16:53:25 +0700

    add task41 final

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+Console.Clear();
+
+Console.WriteLine("введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine("введите число: ");
+
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number > 0)
+
+    count++;
+}
+
+Console.WriteLine($"Количество положительных чисел : {count}");
