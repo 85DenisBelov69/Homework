2bac51f0cef340c00d55f83b30b1798f6a44a703
DenisBelov <olgabelova@192.168.0.4>
2023-02-22 01:02:34 +0700

    add task52 final

--- task52/Program.cs
+++ task52/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+int[,] result = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+result[i, j] = new Random().Next(minValue, maxValue + 1);
+}
+}
+return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+for (int i = 0; i < inArray.GetLength(0); i++)
+{
+for (int j = 0; j < inArray.GetLength(1); j++)
+{
+Console.Write($"{inArray[i, j]}\t ");
+}
+Console.WriteLine();
+}
+}
+
+void SumDivQuantity(int[,] inArray)
+{
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+        double sum = 0;
+        for(int i = 0; i < inArray.GetLength(0); i++)
+        {
+            sum += inArray[i,j];
+        }
+        double SumDivQty = Math.Round(sum/inArray.GetLength(0),2);
+        Console.Write($"{SumDivQty},  ");
+    }
+
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(row, col, -10, 10);
+PrintArray(array);
+Console.WriteLine();
+SumDivQuantity(array);
+
+
+
