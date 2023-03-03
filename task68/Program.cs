379b85467a05adc5327041d3c6e4111f19180634
DenisBelov <olgabelova@192.168.0.4>
2023-03-03 15:40:56 +0700

    task68 final2

--- task68/Program.cs
+++ task68/Program.cs
@@ -7,8 +7,8 @@ Console.Clear();
 int Ackerman(int m, int n)
 {
     if (m == 0) return n + 1;
-    else if (n == 0) return Ackerman(m-1,1);
-    else return Ackerman(m-1, Ackerman(m,n - 1));
+    else if (n == 0) return Ackerman(m - 1, 1);
+    else return Ackerman(m - 1, Ackerman(m, n - 1));
 }
 
 Console.WriteLine("введите число m: ");
@@ -16,4 +16,4 @@ int m = int.Parse(Console.ReadLine()!);
 Console.WriteLine("введите число n: ");
 int n = int.Parse(Console.ReadLine()!);
 Console.WriteLine("A(m,n): ");
-Console.WriteLine(Ackerman(m,n));
+Console.WriteLine(Ackerman(m, n));
