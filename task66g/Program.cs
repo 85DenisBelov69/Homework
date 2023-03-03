df2968b56902a6cd7290f46c6788e648c59a9eb3
DenisBelov <olgabelova@192.168.0.4>
2023-03-03 15:40:08 +0700

    task66 final2

--- task66g/Program.cs
+++ task66g/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу,
- //которая найдёт сумму натуральных элементов в промежутке от M до N.
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 
 Console.Clear();
@@ -7,18 +7,18 @@ Console.Clear();
 int Recursion(int N, int M)
 {
     int sum = 0;
-    if (N == M ) return N;
+    if (N == M) return N;
     else
-    {         
-    
-     return sum = N + Recursion(N+1, M);
-       
-    }  
-    
+    {
+
+        return sum = N + Recursion(N + 1, M);
+
+    }
+
 }
 Console.WriteLine("введите число от");
 int N = int.Parse(Console.ReadLine()!);
 Console.WriteLine("введите число до  ");
 int M = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Сумма натуральных элементов:");
-Console.WriteLine(Recursion(N,M));
+Console.WriteLine(Recursion(N, M));
