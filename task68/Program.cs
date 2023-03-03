2dd8518fe8014cc6b1e7c5963a35c489398e795b
DenisBelov <olgabelova@192.168.0.4>
2023-03-03 15:38:03 +0700

    add task68 final

--- task68/Program.cs
+++ task68/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n
+
+
+Console.Clear();
+
+int Ackerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackerman(m-1,1);
+    else return Ackerman(m-1, Ackerman(m,n - 1));
+}
+
+Console.WriteLine("введите число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("A(m,n): ");
+Console.WriteLine(Ackerman(m,n));
