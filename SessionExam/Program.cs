49f9a6f545388bb763b5bc52b82af2a53cbb3aea
DenisBelov <olgabelova@192.168.0.4>
2023-03-11 12:40:13 +0700

    add massivs

--- SessionExam/Program.cs
+++ SessionExam/Program.cs
@@ -1,2 +1,5 @@
 ﻿// Из имеющегося массива сформировать новый с строками менее 3 символов
 
+Console.Clear();
+string[] GetArray = new string[5] { "111", "2", "333", "aaa", "wxwx" };
+string[] NewArray = new string[GetArray.Length];
