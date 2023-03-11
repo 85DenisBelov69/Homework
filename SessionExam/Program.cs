b30889487bdd63cea5600baa4022390ecab116b9
DenisBelov <olgabelova@192.168.0.4>
2023-03-11 12:41:21 +0700

    add void function

--- SessionExam/Program.cs
+++ SessionExam/Program.cs
@@ -3,3 +3,18 @@
 Console.Clear();
 string[] GetArray = new string[5] { "111", "2", "333", "aaa", "wxwx" };
 string[] NewArray = new string[GetArray.Length];
+
+void CountNumbers(string[] GetArray, string[] NewArray)
+{
+    int count = 0;
+
+    for (int i = 0; i < GetArray.Length; i++)
+    {
+        if (GetArray[i].Length <= 3)
+        {
+            NewArray[count] = GetArray[i];
+        
+            count++;
+        }
+    }
+}
