6545ef7d970fd9753fb5e3d773b1a893463374e0
DenisBelov <olgabelova@192.168.0.4>
2023-03-11 12:42:11 +0700

    add void function printarray

--- SessionExam/Program.cs
+++ SessionExam/Program.cs
@@ -18,3 +18,12 @@ void CountNumbers(string[] GetArray, string[] NewArray)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]},  ");
+    }
+    Console.WriteLine();
+}
